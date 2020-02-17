ef5967e27dd527c6013e6f291968caca97793768
Tess Lameyer <tjlameyer@gmail.com>
2020-02-17 15:40:06 -0800

    anagramsReducer tabled for now

--- src/reducers/anagramsReducer.js
+++ src/reducers/anagramsReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_ANAGRAMS_LOADING } from '../actions/anagramListActions';
+import { FETCH_ANAGRAMS_LOADING, FETCH_ANAGRAMS } from '../actions/anagramListActions';
 
 export default function reducer(state, action) {
   switch(action.type) {
     case FETCH_ANAGRAMS_LOADING:
       return { ...state, loading: true };
+    case FETCH_ANAGRAMS:
+      return { ...state, loading: false, anagrams: action.payload };
       
     default: 
       return state;
