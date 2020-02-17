0f3ff3f03a0b491f92577c3dcbfb0344a719b803
Tess Lameyer <tjlameyer@gmail.com>
2020-02-17 15:34:02 -0800

    FETCH_ANAGRAMS_LOADING test passing

--- src/reducers/anagramsReducer.js
+++ src/reducers/anagramsReducer.js
@@ -1,6 +1,10 @@
+import { FETCH_ANAGRAMS_LOADING } from '../actions/anagramListActions';
 
 export default function reducer(state, action) {
   switch(action.type) {
+    case FETCH_ANAGRAMS_LOADING:
+      return { ...state, loading: true };
+      
     default: 
       return state;
   }
