98facccb5efeb5ad68ae71291275ca7871d707c4
Tess Lameyer <tjlameyer@gmail.com>
2020-02-17 14:10:07 -0800

    configured search for min max characters

---
+++ src/components/anagramList/anagram/Anagram.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Anagram = () => {
+
+  return (
+
+  );
+};
+
+export default Anagram; 
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -10,7 +10,7 @@ const Search = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="word to anagram" onChange={({ target }) => setWordToAnagram(target.value)} /> 
+      <input type="text" min='2' max='10' maplaceholder="word to anagram" onChange={({ target }) => setWordToAnagram(target.value)} /> 
       <button>anagram!</button>
     </form>
   );
