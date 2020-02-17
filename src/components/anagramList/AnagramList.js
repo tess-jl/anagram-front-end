745e1dfc704da33f368e249a411891019ba8597d
Tess Lameyer <tjlameyer@gmail.com>
2020-02-17 13:38:18 -0800

    drafted AnagramList component with hardcoded anagram array for now

---
+++ src/components/anagramList/AnagramList.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import Anagram from './anagram/Anagram';
+
+const AnagramList = () => {
+  //to change later
+  const anagramsArray = ['hi', 'ih'];
+
+  const anagramsForList = anagramsArray.map(anagram => (
+    <li key={anagram}>
+      <Anagram {...anagram} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {anagramsForList}
+    </ul>
+  );
+};
+
+export default AnagramList;
